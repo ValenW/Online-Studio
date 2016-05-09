13fe69af5947373dbcae5ab92b9089fd19e86c28
Lin Yiting <linytsysu@163.com>
2016-05-09 21:10:02 +0800

    initial commit: the structure of the project

---
+++ Gruntfile.js
@@ -0,0 +1,51 @@
+module.exports = function (grunt) {
+  var path = require('path');
+  grunt.initConfig({
+    copy: {
+      main: {
+        expand: true,
+        cwd: 'src/',
+        src: '**',
+        dest: 'bin/'
+      }
+    },
+    clean: {
+      build: {
+        src: ["bin"]
+      }
+    },
+    express: {
+      options: {
+        port: 3000
+      },
+      dev: {
+        options: {
+          script: 'bin/bin/www',
+          livereload: true
+        }
+      }
+    },
+    watch: {
+      options: {
+        livereload: true
+      },
+      copy: {
+        files: ['src/**/*.*'],
+        tasks: ['copy:main']
+      },
+      express: {
+        files: ['src/**/*.*'],
+        tasks: ['express:dev'],
+        options: {
+          spawn: false
+        }
+      }
+    }
+  });
+  grunt.loadNpmTasks('grunt-contrib-copy');
+  grunt.loadNpmTasks('grunt-contrib-clean');
+  grunt.loadNpmTasks('grunt-contrib-watch');
+  grunt.loadNpmTasks('grunt-express-server');
+
+  grunt.registerTask('default', ['clean','copy','express', 'watch']);
+}
