f9fcd9a79340eac5a19bdfac776d946d2e7bdd51
ValenW <wwl0816@qq.com>
2016-05-26 09:11:56 +0800

    add mailsender, add email field in signup.jade

---
+++ src/models/mailConfig.js
@@ -0,0 +1,11 @@
+module.exports = {
+    // service: 'QQ',
+	host: "smtp.sina.cn",
+	port: 25,
+    auth: {
+        user: 'meliodas@sina.com',
+        pass: 'Meliodas'
+    },
+    logger: true, // log to console
+    debug: true // include SMTP traffic in the logs
+}
