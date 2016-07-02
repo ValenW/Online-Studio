f3b3eca5e10428896659d322c1b56a815d51bfa4
SimbaScorpio <425830509@qq.com>
2016-07-02 17:01:34 +0800

    Update editor's data interface.

--- src/public/javascripts/editor/data.js
+++ src/public/javascripts/editor/data.js
@@ -1,11 +1,21 @@
 window.addEventListener("load", init, false);
 
 function init() {
-	window.patternList = new Array();
-	window.addNote = function(pattern, key, head, tail, played) {
-		var _pattern = pattern - 1;
-		if (patternList[_pattern] == null) {
-			patternList[_pattern] = new Array();
+	window.channelList = new Array();
+	window.spectrum = {
+		"spectrumId": 0,
+		"tempo": 110,
+		"volume": 10,
+		"channels": channelList,
+		"createDate": new Date(),
+		"lastModificationDate": new Date()
+	}
+
+	/* interface */
+	window.addNote = function(channel, key, head, tail, played) {
+		var _channel = channel - 1;
+		if (channelList[_channel] == null) {
+			channelList[_channel] = new Array();
 		}
 		note = {
 			key: key,
@@ -13,23 +23,67 @@ function init() {
 			tail: tail,
 			played: played
 		}
-		patternList[_pattern].push(note);
-		return patternList[_pattern].length - 1;
+		channelList[_channel].push(note);
+		return channelList[_channel].length - 1;
 	}
 
-	window.updateNote = function(pattern, index, key, head, tail, played) {
-		var _pattern = pattern - 1;
-		if (patternList[_pattern] == null) return;
-		patternList[_pattern][index].key = key;
-		patternList[_pattern][index].head = head;
-		patternList[_pattern][index].tail = tail;
-		patternList[_pattern][index].played = played;
+	window.updateNote = function(channel, index, key, head, tail, played) {
+		var _channel = channel - 1;
+		if (channelList[_channel] == null) return;
+		channelList[_channel][index].key = key;
+		channelList[_channel][index].head = head;
+		channelList[_channel][index].tail = tail;
+		channelList[_channel][index].played = played;
 	}
 
-	window.removeNote = function(pattern, index) {
-		var _pattern = pattern - 1;
-		if (patternList[_pattern] == null) return;
-		patternList[_pattern][index] = null;
+	window.removeNote = function(channel, index) {
+		var _channel = channel - 1;
+		if (channelList[_channel] == null) return;
+		channelList[_channel][index] = null;
+	}
+
+	window.save = function() {
+		spectrum.lastModificationDate = new Date();
+		var channels = new Array();
+		for (var i = 0; i < channelList.length; ++i) {
+			if (channelList[i] != null) {
+				channels[i] = new Array();
+				for (var j = 0; j < channelList[i].length; ++j) {
+					note = {
+						key: channelList[i][j].key,
+						head: channelList[i][j].head,
+						tail: channelList[i][j].tail
+					}
+					channels[i].push(note);
+				}
+			}
+		}
+		spectrum.channels = channels;
+		$.post(
+			"/editor/post",
+			{"spectrum": spectrum},
+			function(data) {
+				console.log(data);
+				spectrum = data;
+			}
+		);
+	}
+
+	// request spectrum
+	var param = location.pathname.split('/editor/');
+	if (param.length > 1 && param[1].length > 0) {
+		var id = param[1];
+		$.get(
+			"/editor/"+id,
+			function(data) {
+				console.log(data);
+				spectrum = data;
+				window.setTempo(spectrum.tempo);
+				window.initEditor();
+			}
+		);
+	} else {
+		window.initEditor();
 	}
 }
 
