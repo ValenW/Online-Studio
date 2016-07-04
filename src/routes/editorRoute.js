3c2a2d2b8afeba967192fc49c486aeb772129013
MchCyLh <MchCyLh@gmail.com>
2016-07-04 09:57:30 +0800

    add relationship between User & Music created for Spectrum

--- src/routes/editorRoute.js
+++ src/routes/editorRoute.js
@@ -1,6 +1,8 @@
 var express = require('express');
 var router = express.Router();
 var Spectrum = require('../models/Spectrum');
+var Music = require('../models/Music');
+var User = require('../models/User')
 
 router.route('/')
 .get(function(req, res, next) {
@@ -27,7 +29,7 @@ router.route('/')
 		});
 
 	}
-	
+
 });
 
 // if spectrum_param has no _id, the new method create one.
@@ -37,20 +39,48 @@ router.route('/save')
 	spectrum_param = JSON.parse(req.body.spectrum);
 
 	if (spectrum_param._id == undefined) {	// create Spectrum document
+		// create Music document and set relationship between User and Music
+		user = req.session.user;
+		// create Spectrum
 		spectrum = new Spectrum(spectrum_param);
-		spectrum.save(function(err, spectrumAffected, numAffected) {
+		spectrum.save();
+
+		// cascaded in save method ?
+		// create Music info for Spectrum
+		music = new Music({
+			spectrum: spectrum,
+			name: 'music',
+			author: user,
+			cover: 'none',
+			date: new Date(),
+			tags: [],
+			ranks: [],
+			comments: [],
+			listenN: 0,
+			collectN: 0,
+			commentN: 0,
+			shareN: 0
+		});
+		music.save();
+
+		// set relationship
+		user.musics.push(music);
+
+		User.update({_id: user._id}, user, {}, function(err, info) {
 			if (err) {
-				console.log ('Error in /editor/save subopertion creating Spectrum.');
-			} else {
-				if (spectrum._id == spectrumAffected._id) {
-					res.json({
-						_id: spectrumAffected._id
-					});
-				} else {
-					console.log ('Something unexpected happened in /editor/save.' );
-				}
+				console.log ('Error in /editor/save updating User.');
+			}  else {
+				// Music.find({},function(err, musics) {
+				// 	console.log(musics);
+				// });
+				// User.find({}, function(err, users) {
+				// 	console.log (users);
+				// });
+				// console.log (info);
+				console.log('Update User with Music successfully.');
 			}
 		});
+
 	} else {	// update Spectrum document
 		Spectrum.update({_id: spectrum_param._id}, spectrum_param, {}, function(err, info) {
 			if (err) {
