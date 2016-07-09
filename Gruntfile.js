4140170d81075690adc52b054bab9877f0799417
Lin Yiting <linytsysu@163.com>
2016-07-09 13:19:09 +0800

    grunt

--- Gruntfile.js
+++ Gruntfile.js
@@ -7,11 +7,37 @@ module.exports = function (grunt) {
         cwd: 'src/',
         src: '**',
         dest: 'bin/'
+      },
+      deploy: {
+        options: {
+          process: function(content, srcpath) {
+            var jsReg = /javascripts\/(?:(?:\w+)\/)*(\w+)\.js/g
+            var cssReg = /stylesheets\/(?:(?:\w+)\/)*(\w+)\.css/g
+            console.log(content.match(jsReg));
+            console.log(content.match(cssReg));
+            // var jsResult = "javascripts/"+content.match(jsReg)[1]+"min"+".js";
+            // console.log(jsResult);
+            // content.replace(jsReg, jsResult);
+          }
+        },
+        expand: true,
+        cwd: 'src',
+        src: '**',
+        dest: 'bin'
       }
     },
     clean: {
       build: {
         src: ["bin"]
+      }
+    },
+    concat: {
+      options: {
+        separator: ";"
+      },
+      lib: {
+        src: ['./src/public/libs/jquery/jquery-1.12.3.min.js', './src/public/libs/semantic-ui/semantic.min.js'],
+        dest: 'bin/public/libs/lib.min.js'
       }
     },
     express: {
@@ -41,7 +67,10 @@ module.exports = function (grunt) {
   grunt.loadNpmTasks('grunt-contrib-copy');
   grunt.loadNpmTasks('grunt-contrib-clean');
   grunt.loadNpmTasks('grunt-contrib-watch');
+  grunt.loadNpmTasks('grunt-contrib-concat');
   grunt.loadNpmTasks('grunt-express-server');
 
-  grunt.registerTask('default', ['clean','copy','express', 'watch']);
+  grunt.registerTask('build', ['clean', 'copy:main', 'concat:lib']);
+  grunt.registerTask('deploy', ['clean', 'copy:deploy', 'concat:lib']);
+  grunt.registerTask('default', ['clean','copy:main','concat:lib','express', 'watch']);
 }
