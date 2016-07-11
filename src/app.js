18c89509d1344f4e3deb59049e3922060c31e5d9
Lin Yiting <linytsysu@163.com>
2016-07-11 23:31:04 +0800

    icon

--- src/app.js
+++ src/app.js
@@ -22,6 +22,9 @@ app.set('view engine', 'jade');
 
 // uncomment after placing your favicon in /public
 //app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
+
+app.use(favicon(__dirname+"/public/icons/favicon.ico"));
+
 app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
