d23df608fdcf8ce267e00c3fdbd1d4da0dc772fd
MchCyLh <MchCyLh@gmail.com>
2016-07-14 00:08:30 +0800

    construct code in controllers/editor.js&models/user.js

--- src/models/user.js
+++ src/models/user.js
@@ -13,6 +13,46 @@ UserSchema = new mongoose.Schema({
     createDate: Date
 });
 
+UserSchema.method('addOriginalMusic', function(music_id) {
+    this.original_musics.push(music_id);
+    console.log ('Add original music ', music_id, ' for user ', this._id);
+});
+
+UserSchema.method('addDerivativeMusic', function(music_id) {
+    this.derivative_musics.push(music_id);
+    console.log ('Add derivative music ', music_id, ' for user ', this._id);
+});
+
+UserSchema.static('addOriginalMusicForUser', function(info, callback) {
+    // console.log(info);
+    this.findOne({
+        _id: info.user_id
+    }, function(err, user) {
+        if (err) {
+            console.log('Error in User.addOriginalMusicForUser method.');
+        } else {
+            user.addOriginalMusic(info.music_id);
+            user.save();
+            callback(true);
+        }
+    });
+});
+
+UserSchema.static('addOriginalMusicForUser', function(info, callback) {
+    // console.log(info);
+    this.findOne({
+        _id: info.user_id
+    }, function(err, user) {
+        if (err) {
+            console.log('Error in User.addDerivativeMusicForUser method.');
+        } else {
+            user.addDerivativeMusic(info.music_id);
+            user.save();
+            callback(true);
+        }
+    });
+});
+
 
 UserSchema.static('findByUsername', function(username, callback) {
     return this.find({username: username}, callback);
